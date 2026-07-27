9850f780f5ebd00f7555b581f50df850deddc706
M.Djot <hafidatchiegmou@gmail.com>
2026-07-27 15:52:40 +0100

    learning how to use if statements

--- Day6/script.js
+++ Day6/script.js
@@ -2,4 +2,13 @@
     document.getElementById("demo").innerText = "Paragraph changed.";
  }
   document.write(5+6);
-  
+  (function () {
+  "use strict";
+  /* Start of your code */
+  function greetMe(yourName) {
+    alert(`Hello ${yourName}`);
+  }
+
+  greetMe("World");
+  /* End of your code */
+})();
