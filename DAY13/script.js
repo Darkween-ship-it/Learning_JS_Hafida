8c5eb5f4ea3b726d393c7a2b3da96a9cb8969307
M.Djot <hafidatchiegmou@gmail.com>
2026-08-15 17:25:54 +0100

    declaring all the js variables

--- DAY13/script.js
+++ DAY13/script.js
@@ -4,3 +4,12 @@ let img = document.getElementById('.bird-1');
 
 let bird_props = bird.getBoundingClientRect();
 
+let background = document.querySelector('.background').getBoundingClientRect();
+let scor_val = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let score_title = document.querySelector('.score_title');
+
+let game_state = 'start';
+img.style.display = 'none';
+message.classList.add('messageStyle');
+
