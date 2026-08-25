d1b9f718497015cca0065d5acba2c7dcb3179a01
M.Djot <hafidatchiegmou@gmail.com>
2026-08-25 12:28:14 +0100

    fixed the issue withfetching the RES API data. forEach intead on foreach and post.body instead of post_body

--- DAY 15/script.js
+++ DAY 15/script.js
@@ -1,11 +1,15 @@
 async function load_blog_post() {
     const blog_data = await fetch ("https://jsonplaceholder.typicode.com/posts");
-    const blog_parse = await blog_data.JSON();
+    const blog_parse = await blog_data.json();
 
     blog_parse.forEach(post => {
-        myDisplayer(`<h3> ${post.title}</h3> <p> ${post_body}</p> </h3>`);
+        myDisplayer(`<h3>${post.title}</h3><p>${post.body}</p><hr>`);
     });
     
 }
 
-funct
+function myDisplayer(text){
+    document.getElementById('demo').innerHTML += text;
+}
+
+load_blog_post();
