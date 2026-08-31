379d441361ee317e64e70fe28d388c8727871294
M.Djot <hafidatchiegmou@gmail.com>
2026-08-31 13:14:58 +0100

    applying ES6 features like maps, sets, generators to create a more prformant task scheduler

---
+++ DAY16/Task.js
@@ -0,0 +1,11 @@
+const readline = recquire("readline");
+
+const rl = readline.createInterface({
+    input:ProcessingInstruction.stdin,
+    output: ProcessingInstruction.stdout,
+});
+
+rl.question("Enter your task:", (taskName) =>{
+    console.log(`your task is : ${taskName}`);
+    rl.close();
+});
