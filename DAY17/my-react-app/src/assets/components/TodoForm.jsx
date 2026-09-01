23bec3c5aa9b4b8870b9d6706a54fe37057f7c96
M.Djot <hafidatchiegmou@gmail.com>
2026-09-01 12:10:03 +0100

    creatin the TodoForm.jsx component

---
+++ DAY17/my-react-app/src/assets/components/TodoForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function TodoForm({ onAddTodo }) {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (text.trim() === "") return;
+
+    onAddTodo(text);
+    setText("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+export default TodoForm;
+
